8119d47e098c7f2d12ca85b77e5a91ebd5a63b3f
Zack Moore <ormico@users.noreply.github.com>
2019-07-06 15:18:49 -0400

    add cli

---
+++ AzureDevopsPipelineTest/Ormico.AzureDevopsPipelineTest.CLI/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Ormico.AzureDevopsPipelineTest.CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var c1 = new Ormico.AzureDevopsPipelineTest.Class1();
+            Console.WriteLine(c1.ToString());
+        }
+    }
+}
